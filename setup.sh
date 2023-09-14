e7c90458e0ba46506b822adf78e555274f76df97
napoknot21 <napoknot21@protonmail.com>
2023-09-14 11:07:28 +0200

    initial init

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+python3 -m venv .
+
+source ./bin/activate
+
+sed -i "s/false/true/g" pyvenv.cfg
+
+export PATH=$PATH:$PWD/bin/
+
+python3 -m pip install --user ./extras/PyRat
+
+./bin/python3 -c "import pyrat; pyrat.PyRat.setup_workspace()"
